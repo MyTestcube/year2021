1d677a0cec4aa6ec9b4c6d7ae4ad39b694aef891
cyz <53589456+nimmbaba@users.noreply.github.com>
2021-07-21 17:19:12 +0800

    2021.07.21 17:19

---
+++ src/main/java/Moon7/RunoobTest.java
@@ -0,0 +1,24 @@
+package Moon7;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RunoobTest {
+    public static void main(String[] args) {
+
+        // 创建集合
+        ArrayList<String> sites = new ArrayList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+
+        // 获取迭代器
+        Iterator<String> it = sites.iterator();
+
+        // 输出集合中的所有元素
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
